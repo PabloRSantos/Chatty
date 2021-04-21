5dfd2049532c1f66fcbe8128eb85dbb67ff53ce8
PabloRSantos <pablorsantos15@gmail.com>
2021-04-21 17:01:31 -0300

    third day

--- src/controller/SettingsController.ts
+++ src/controller/SettingsController.ts
@@ -3,13 +3,19 @@ import { SettingsService } from "../services/SettingsService";
 
 class SettingsController {
   async create(request: Request, response: Response) {
-    const { username, chat } = request.body;
-
-    const settingsService = new SettingsService();
-
-    const settings = await settingsService.create({ username, chat });
-
-    return response.json(settings);
+    try {
+      
+      
+      const { username, chat } = request.body;
+      
+      const settingsService = new SettingsService();
+      
+      const settings = await settingsService.create({ username, chat });
+      
+      return response.json(settings);
+    } catch (error) {
+      return response.status(400).json({error: error.message})
+    }
   }
 }
 
